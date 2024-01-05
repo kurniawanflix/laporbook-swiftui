17110f3a7c10d44d91c63254c1421998ea40786a
kurniawanflixxx <kurniawanflix@campaign.com>
2024-01-05 20:05:52 +0700

    Initial Commit

---
+++ laporbook-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  laporbook-swift
+//
+//  Created by Muhammad David Kurniawan on 05/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ laporbook-swift/laporbook_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  laporbook_swiftApp.swift
+//  laporbook-swift
+//
+//  Created by Muhammad David Kurniawan on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct laporbook_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
